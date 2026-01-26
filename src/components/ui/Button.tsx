4fcff5c9b14b80f3c9079873c88582a94e7e3022
ely-anakhe <lassmanely@gmail.com>
2026-01-26 19:17:43 +0000

    style: Use serif font for buttons

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -10,7 +10,7 @@ function cn(...inputs: ClassValue[]) {
 }
 
 const buttonVariants = cva(
-    "inline-flex items-center justify-center whitespace-nowrap rounded-sm text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 duration-300",
+    "inline-flex items-center justify-center whitespace-nowrap rounded-sm text-sm font-serif ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 duration-300",
     {
         variants: {
             variant: {
